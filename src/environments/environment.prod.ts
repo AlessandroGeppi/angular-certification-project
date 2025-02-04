7c423f128a6aca0f009fdb8b693812694860bc1f
Alessandro Geppi <alessandro.geppi@mminformatica.it>
2025-02-04 14:44:58 +0100

    changed prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   test: false,
-  baseUrl: "https://angular-certification-project-omega.vercel.app",
+  baseUrl: "https://ng-job-search-ag.vercel.app",
 };
